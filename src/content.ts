1c807135312bb40e4e4f03d40b4f9b66feebae6e
Aly Thobani <athobani9@gmail.com>
2025-01-19 11:49:09 -0800

    refactor: use recursion and setTimeout instead of setInterval

--- src/content.ts
+++ src/content.ts
@@ -3,80 +3,41 @@ import { logMessage } from "./logger";
 const INTERVAL_MS = 3000;
 const MAX_NUM_CHECKS = 5;
 
-type IntervalState = {
-  id: NodeJS.Timeout | null;
-  numChecks: number;
-};
-
-type State = {
-  viewMoreCommentsIntervalState: IntervalState;
-  moreRepliesIntervalState: IntervalState;
-};
+const BUTTON_NAMES_TO_CLICK = [
+  "View more comments",
+  "more replies",
+  "1 more reply",
+];
 
 function main(): void {
-  const state = initializeState();
-  clickViewMoreComments(state.viewMoreCommentsIntervalState);
-  clickMoreRepliesButtons(state.moreRepliesIntervalState);
-  state.viewMoreCommentsIntervalState.id = setInterval(
-    () => clickViewMoreComments(state.viewMoreCommentsIntervalState),
-    INTERVAL_MS
-  );
-  state.moreRepliesIntervalState.id = setInterval(
-    () => clickMoreRepliesButtons(state.moreRepliesIntervalState),
-    INTERVAL_MS
-  );
+  clickAllButtons();
 }
 
-function initializeState(): State {
-  return {
-    viewMoreCommentsIntervalState: { id: null, numChecks: 0 },
-    moreRepliesIntervalState: { id: null, numChecks: 0 },
-  };
-}
-
-function clickViewMoreComments(intervalState: IntervalState): void {
-  const didStop = maybeStopInterval(intervalState);
-  if (didStop) return;
-  intervalState.numChecks += 1;
+function clickAllButtons(numChecksSoFar = 0): void {
+  if (numChecksSoFar >= MAX_NUM_CHECKS) {
+    return;
+  }
+  logMessage("Finding buttons to click");
   document
     .querySelectorAll<HTMLSpanElement>("button span")
     .forEach((buttonSpan) => {
-      if (!buttonSpan.innerText.includes("View more comments")) {
+      if (!isButtonToClick(buttonSpan)) {
         return;
       }
       clickButtonSpan(buttonSpan);
     });
+  setTimeout(() => clickAllButtons(numChecksSoFar + 1), INTERVAL_MS);
 }
 
-function clickMoreRepliesButtons(intervalState: IntervalState): void {
-  const didStop = maybeStopInterval(intervalState);
-  if (didStop) return;
-  intervalState.numChecks += 1;
-  document
-    .querySelectorAll<HTMLSpanElement>("button span.text-secondary-weak")
-    .forEach((buttonSpan) => {
-      if (!buttonSpan.innerText.includes("more replies")) {
-        return;
-      }
-      clickButtonSpan(buttonSpan);
-    });
+function isButtonToClick(buttonSpan: HTMLSpanElement): boolean {
+  return BUTTON_NAMES_TO_CLICK.some((name) =>
+    buttonSpan.innerText.includes(name)
+  );
 }
 
 function clickButtonSpan(buttonSpan: HTMLSpanElement): void {
   logMessage(`Clicking '${buttonSpan.innerText.trim()}' button`);
   buttonSpan.click();
-}
-
-function maybeStopInterval(intervalState: IntervalState): boolean {
-  if (intervalState.numChecks < MAX_NUM_CHECKS) {
-    return false;
-  }
-  if (intervalState.id === null) {
-    return true;
-  }
-  clearInterval(intervalState.id);
-  intervalState.id = null;
-  return true;
 }
 
 main();
